4293ce85816dc0fb42a917290eebadb6f04bb370
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-09-01 15:49:38 +0200

    fix: Add chmod

--- debugging/factorial_recursive.py
+++ debugging/factorial_recursive.py
@@ -4,18 +4,13 @@ import sys
 
 def factorial(n):
     """
-    Calculate the factorial of a non-negative integer using recursion.
+    Calculate the factorial of a given number using recursion.
 
-    Parameters
-    ----------
-    n : int
-        The number for which the factorial is to be calculated.
-        Must be a non-negative integer.
+    Parameters:
+        n (int): A non-negative integer whose factorial is to be computed.
 
-    Returns
-    -------
-    int
-        The factorial of the input number `n`.
+    Returns:
+        int: The factorial of the input number n.
     """
     if n == 0:
         return 1
@@ -23,6 +18,5 @@ def factorial(n):
         return n * factorial(n-1)
 
 
-if __name__ == "__main__":
-    f = factorial(int(sys.argv[1]))
-    print(f)
+f = factorial(int(sys.argv[1]))
+print(f)
